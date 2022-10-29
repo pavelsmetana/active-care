3f3fe8bab82a178a5d223a0dc32adbe67cb1f188
Andrey <anedx@yandex.ru>
2022-10-29 21:35:31 +0400

    moved to develop

---
+++ controller/Controller.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__."/../model/model.php";
+
+
+class Controller
+{
+    public function index(){
+        $model = new Model();
+        $products = $model->list();
+        include __DIR__ . "/../view/view.php";
+    }
+
+}
+
---
+++ footer.php
@@ -0,0 +1,4 @@
+
+<footer>
+    <p>Author: Pavel Smetana.  <a href="mailto:pavelsmetana88@gmail.com">pavelsmetana88@gmail.com</a></p>
+</footer>
